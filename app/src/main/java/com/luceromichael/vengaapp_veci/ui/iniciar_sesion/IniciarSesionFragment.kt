a7654124daa63d373de7b229b53617475751c19b
ShadowNK <inchiglemanicolax@gmail.com>
2020-09-13 00:58:07 -0500

    reload1

--- app/src/main/java/com/luceromichael/vengaapp_veci/ui/iniciar_sesion/IniciarSesionFragment.kt
+++ app/src/main/java/com/luceromichael/vengaapp_veci/ui/iniciar_sesion/IniciarSesionFragment.kt
@@ -1,4 +1,4 @@
-package com.luceromichael.vengaapp_veci.ui.home
+package com.luceromichael.vengaapp_veci.ui.iniciar_sesion
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.luceromichael.vengaapp_veci.R
 
-class HomeFragment : Fragment() {
+class IniciarSesionFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var galleryViewModel: IniciarSesionViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        galleryViewModel =
+                ViewModelProviders.of(this).get(IniciarSesionViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
+        val textView: TextView = root.findViewById(R.id.text_gallery)
+        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
