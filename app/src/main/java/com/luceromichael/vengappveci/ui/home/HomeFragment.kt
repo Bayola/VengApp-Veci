db23ab8e485f0cff2d5c7a0bb2b3e0c7b8adaa1e
anderson Lucero <andersonlucero@hotmail.com>
2020-09-13 23:56:03 -0500

    fragment home with glide v2

--- app/src/main/java/com/luceromichael/vengappveci/ui/home/HomeFragment.kt
+++ app/src/main/java/com/luceromichael/vengappveci/ui/home/HomeFragment.kt
@@ -1,6 +1,5 @@
 package com.luceromichael.vengappveci.ui.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,11 +8,11 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.luceromichael.vengappveci.ProductoHomeAdapter
 import com.luceromichael.vengappveci.R
-import com.luceromichael.vengappveci.ui.carrito.CarritoFragment
 import kotlinx.android.synthetic.main.fragment_home.*
 
 
@@ -43,8 +42,17 @@ class HomeFragment : Fragment() {
             textView.text = it
 
             productos = getProductsLinks()!!
-            productoHomeAdapter = ProductoHomeAdapter(this.requireContext(), productos, R.layout.item_producto)
-            recicleViewHome.layoutManager = LinearLayoutManager(activity)
+            productoHomeAdapter = ProductoHomeAdapter(
+                this.requireContext(),
+                productos,
+                R.layout.item_producto
+            )
+
+
+            recicleViewHome.layoutManager = GridLayoutManager(this.requireContext(), 3)
+
+            //recicleViewHome.layoutManager = LinearLayoutManager(activity)
+
             recicleViewHome.setAdapter(productoHomeAdapter)
 
             imageViewCarrito.setOnClickListener {
@@ -86,3 +94,4 @@ class HomeFragment : Fragment() {
 
 
 }
+
