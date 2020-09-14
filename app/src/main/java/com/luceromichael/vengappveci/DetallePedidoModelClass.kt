6d7b100bf11850bc56e12cbcc1f26a8869d659cb
Oscar Umatambo <oscar.umatambo@epn.edu.ec>
2020-09-13 22:39:44 -0500

    lista de pedidos

--- app/src/main/java/com/luceromichael/vengappveci/DetallePedidoModelClass.kt
+++ app/src/main/java/com/luceromichael/vengappveci/DetallePedidoModelClass.kt
@@ -4,4 +4,8 @@ data class DetallePedidoModelClass(
     var producto: ProductoModelClass,
     var cant: Int,
     var subTotal: Float
-)
+){
+//    fun getNombreProducto(): String? {
+//        return this.producto.name
+//    }
+}
